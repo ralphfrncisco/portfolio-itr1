73683d952292db444b36cbadc655821220311b5c
ralphfrncisco <ralphfrncisco@gmail.com>
2026-03-31 15:38:44 +0800

    tapos naaaaa

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <div className = "py-4 z-2 sticky top-0 bg-[#0B0B0B]/70 border-b border-white/20 backdrop-blur-xs">
         <div className = "max-w-6xl flex items-center justify-between w-full mx-auto">
             <div>
-                <h1 className = "text-xl tracking-wider font-extrabold brand-name">Ralph F<span className = "text-emerald-500">.</span></h1>
+                <h1 className = "text-xl tracking-wide font-bold brand-name">Ralph F<span className = "text-emerald-500">.</span></h1>
             </div>
             <div className = "flex items-center gap-8 normal-text">
                 <a href="#root" className = "text-sm font-normal text-white/80 hover:text-white transition duration-300 cursor-pointer">Home</a>
